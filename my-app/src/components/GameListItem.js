905ec1ead0bdd8bcdb323aa7c2dbb882015cdf08
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2024-06-16 22:23:54 +1200

    refactor: save wheter user has joined game in state

--- my-app/src/components/GameListItem.js
+++ my-app/src/components/GameListItem.js
@@ -3,12 +3,13 @@
  * This component displays the game listings
  */
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext.js';
 import { getDocument, setDocument, deleteDocument, getCollectionIds, subscribeToCollection } from '../proxies/queries.js';
 import LoadingSpinner from './LoadingSpinner.js';
+import { doc } from '@firebase/firestore';
 
 /**
  * GameListItem component.
@@ -18,12 +19,14 @@ import LoadingSpinner from './LoadingSpinner.js';
  * @param {Object} game - The game object.
  * @returns {React.JSX.Element} The rendered GameListItem component.
  */
-export const GameListItem = ({ game }) => {
+export const GameListItem = ({ game, setGamePlayers, setCurrentGame }) => {
     const navigate = useNavigate();
     const { uid } = useAuth();
+    const { gameId } = useParams();
     const [isLoading, setIsLoading] = useState(false);
     const [players, setPlayers] = useState([]);
     const [error, setError] = useState('');
+    const [userIsJoined, setUserIsJoined] = useState(false);
 
     useEffect(() => {
         try {
@@ -39,19 +42,37 @@ export const GameListItem = ({ game }) => {
             console.log("Done getting players.");
             setIsLoading(false);
         }
-    }, []);
+    }, [gameId]);
 
     const handleSnapshot = (snapshot) => {
-        const players = snapshot.docs.map(doc => doc.id);
+        const players = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
         console.log("Players: " + players + " UID: " + uid );  
         setPlayers(players);
+        game.id === gameId && setGamePlayers(players);
         setIsLoading(false);
+        setUserIsJoined(isUserJoined(uid));
     }
 
-    const getPlayers = async (gamePath) => {
-        const playerIds = await getCollectionIds([...gamePath, 'players']);
-        return playerIds || [];
-    }
+    // useEffect(() => {
+    //     try {
+    //         console.log('call');
+    //         setIsLoading(true);
+    //         return subscribeToDocument(['games', gameId], handleGameSnapshot);
+    //     }
+    //     catch (error) {
+    //         console.error("Error getting game: ", error);
+    //         setError("An error occurred. Please try again.");
+    //     }
+    //     finally {
+    //         setIsLoading(false);
+    //     }
+    // }, [gameId]);
+
+    // const handleGameSnapshot = (snapshot) => {
+    //     const game = { id: snapshot.id, ...snapshot.data() };
+    //     setGame(game);
+    //     game.id === gameId && setCurrentGame(game);
+    // }
 
     const joinGame = async (gameId) => {
         setIsLoading(true);
@@ -83,6 +104,10 @@ export const GameListItem = ({ game }) => {
         }
     }
 
+    const isUserJoined = (uid) => {
+        return players.some(player => player.id === uid);
+    }
+
     if (!game) {
         return null;
     }
@@ -98,10 +123,10 @@ export const GameListItem = ({ game }) => {
             </p>
             <p>Player Count: {players?.length}</p>
             <div className='button-group'>
-                <Button variant="primary" onClick={() => joinGame(game.id)} disabled={players?.includes(uid)}>
+                <Button variant="primary" onClick={() => joinGame(game.id)} disabled={userIsJoined}>
                     Join
                 </Button>
-                <Button variant="secondary" onClick={() => leaveGame(game.id)} disabled={!players?.includes(uid)}>
+                <Button variant="secondary" onClick={() => leaveGame(game.id)} disabled={!userIsJoined}>
                     {players?.length !== 1 ? 'Leave' : 'Delete'}
                 </Button>
             </div>
