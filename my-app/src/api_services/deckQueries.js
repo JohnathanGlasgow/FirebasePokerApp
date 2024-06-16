e44703fa206d8e8b37cce3118e1f4bc68d419b2d
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2024-06-16 22:23:10 +1200

    feat: get remaining amount of cards in deck

--- my-app/src/api_services/deckQueries.js
+++ my-app/src/api_services/deckQueries.js
@@ -29,9 +29,28 @@ export const drawCards = async (deckId, count) => {
     try {
         const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${count}`);
         const data = await response.json();
+        console.log(`Drew ${count} cards from deck ${deckId}:`, data.cards.map(card => card.code));
         return data;
     } catch (error) {
         console.error(`Failed to draw ${count} cards:`, error);
+        throw error;
+    }
+}
+
+/**
+ * This function returns number of cards remaining in the deck using https://www.deckofcardsapi.com/api/deck/zegdsdvbbudn/return/
+ * @param {string} deckId - The ID of the deck to draw from.
+ * @returns {Promise<Object>} A Promise that resolves with the card object.
+ */
+
+export const getRemainingCards = async (deckId) => {
+    try {
+        const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=0`);
+        const data = await response.json();
+        console.log(`Remaining cards in deck ${deckId}:`, data.remaining);
+        return data;
+    } catch (error) {
+        console.error(`Failed to get remaining cards:`, error);
         throw error;
     }
 }
