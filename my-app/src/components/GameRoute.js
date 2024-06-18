d5dda3c953ebf2b4e4138a39c647f0184c247c54
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2024-06-18 21:20:37 +1200

    feat: don't render GameBoard until gaem is in state

--- my-app/src/components/GameRoute.js
+++ my-app/src/components/GameRoute.js
@@ -1,16 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GamesCard from './GamesCard';
 import GameBoard from './GameBoard';
 
 export default function GameRoute() {
-    const [players, setPlayers] = useState(null);
-    const [game, setGame] = useState(null);
-  
-    return (
-      <>
-        <GamesCard setGamePlayers={setPlayers} setCurrentGame={setGame} />
-        <GameBoard players={players} game={game} />
-      </>
-    );
-  }
-  
+  const [players, setPlayers] = useState(null);
+  const [game, setGame] = useState(null);
+
+  return (
+    <>
+      <GamesCard setGamePlayers={setPlayers} setCurrentGame={setGame} />
+      {game && <GameBoard players={players} game={game} />}
+    </>
+  );
+}
+
